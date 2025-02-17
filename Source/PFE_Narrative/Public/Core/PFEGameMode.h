a60ff9f9b206a813dbdf62fcbfb46d87ccf41df0
Laurene <laurene.broquet@gmail.com>
2025-02-17 14:23:41 +0100

    create game mode and BP
    set default game mode in property

---
+++ Source/PFE_Narrative/Public/Core/PFEGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PFEGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PFE_NARRATIVE_API APFEGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
