5413e32ac96d7bba88d3a21828489ee086229385
Laurene <laurene.broquet@gmail.com>
2025-03-02 20:55:29 +0100

    add moving platform

---
+++ Source/PFE_Narrative/Public/Platforms/PaperPlatformMoving.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSpriteActor.h"
+#include "PaperPlatformMoving.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PFE_NARRATIVE_API APaperPlatformMoving : public APaperSpriteActor
+{
+	GENERATED_BODY()
+	
+};
