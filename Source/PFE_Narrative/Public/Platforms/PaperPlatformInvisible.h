a52a80baededdaf4c3a209e17179f05a74a63525
Laurene <laurene.broquet@gmail.com>
2025-03-02 20:56:00 +0100

    add invisible platform
    - empty

---
+++ Source/PFE_Narrative/Public/Platforms/PaperPlatformInvisible.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSpriteActor.h"
+#include "PaperPlatformInvisible.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PFE_NARRATIVE_API APaperPlatformInvisible : public APaperSpriteActor
+{
+	GENERATED_BODY()
+	
+};
