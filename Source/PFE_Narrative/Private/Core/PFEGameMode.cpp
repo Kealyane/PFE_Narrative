0f0039ade734e0a8f77d611def45d164a7a7eac0
Laurene <laurene.broquet@gmail.com>
2025-03-29 11:09:03 +0100

    game mode : checkpoints and death

--- Source/PFE_Narrative/Private/Core/PFEGameMode.cpp
+++ Source/PFE_Narrative/Private/Core/PFEGameMode.cpp
@@ -3,3 +3,15 @@
 
 #include "Core/PFEGameMode.h"
 
+APFEGameMode::APFEGameMode()
+{
+
+}
+
+void APFEGameMode::SetCheckpoint(const FVector& CheckpointPosition)
+{
+	LastCheckpointLocation = CheckpointPosition;
+	GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("GameMode::Checkpoint"));
+	UE_LOG(LogTemp, Warning, TEXT("GameMode::Checkpoint"));
+}
+
--- Source/PFE_Narrative/Public/Core/PFEGameMode.h
+++ Source/PFE_Narrative/Public/Core/PFEGameMode.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "PFEGameMode.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FCharacterDeathSignature);
+
 /**
  * 
  */
@@ -13,5 +15,21 @@ UCLASS()
 class PFE_NARRATIVE_API APFEGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	APFEGameMode();
 	
+	UPROPERTY(BlueprintAssignable)
+	FCharacterDeathSignature OnDeath;
+
+	UFUNCTION()
+	void SetCheckpoint(const FVector& CheckpointPosition);
+	UFUNCTION()
+	FVector GetCheckpointPosition() const { return LastCheckpointLocation; }
+
+	UFUNCTION(BlueprintCallable)
+	void LaunchDeathEvent() { OnDeath.Broadcast(); }
+	
+protected:
+	FVector LastCheckpointLocation;
 };
