0c1cc94cfaeeb0afc400d950d0afdca4287f54d4
Laurene <laurene.broquet@gmail.com>
2025-03-31 16:58:50 +0200

    add pick up key

---
+++ Source/PFE_Narrative/Private/Gameplay/KeyPickup.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gameplay/KeyPickup.h"
+
+#include "PaperSpriteComponent.h"
+#include "Core/PFEGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/PFECharacter.h"
+
+AKeyPickup::AKeyPickup()
+{
+}
+
+void AKeyPickup::BeginPlay()
+{
+	Super::BeginPlay();
+	if (APFEGameMode* PFEGameMode = Cast<APFEGameMode>(UGameplayStatics::GetGameMode(this)))
+	{
+		PFEGameMode->OnDeath.AddDynamic(this, &AKeyPickup::InitKey);
+	}
+	InitKey();
+	
+	OnActorBeginOverlap.AddDynamic(this, &AKeyPickup::OnOverlapBegin);
+}
+
+void AKeyPickup::InitKey()
+{
+	GetRenderComponent()->SetVisibility(true);
+	SetActorEnableCollision(true);
+}
+
+void AKeyPickup::OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (OtherActor)
+	{
+		if (APFECharacter* Character = Cast<APFECharacter>(OtherActor))
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, TEXT("AKeyPickup::StoreKey"));
+			Character->StoreKey();
+			SetActorEnableCollision(false);
+			GetRenderComponent()->SetVisibility(false);
+			// FTimerHandle GrabHandle;
+			// GetWorldTimerManager().SetTimer(
+			// 	GrabHandle,
+			// 	[this]() { GetRenderComponent()->SetVisibility(false); },
+			// 	0.5f,
+			// 	false);
+		}
+	}
+}
