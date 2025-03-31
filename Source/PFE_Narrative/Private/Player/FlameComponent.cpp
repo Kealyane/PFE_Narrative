b2cfdd9cf153c05bd10c9249f612af44b1ad5805
Laurene <laurene.broquet@gmail.com>
2025-03-31 11:35:46 +0200

    flame component : handle areas

--- Source/PFE_Narrative/Private/Player/FlameComponent.cpp
+++ Source/PFE_Narrative/Private/Player/FlameComponent.cpp
@@ -38,3 +38,57 @@ void UFlameComponent::UpdateFlameValue(float Value)
 		}
 	}
 }
+
+void UFlameComponent::StartEffect(float Value, float Delay, bool bDecrease, AArea* InAreaRef)
+{
+	// stop current area effect to apply new one
+	if (Areas.Num() > 0)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(FlameEffectTimer);
+	}
+	// store new effect
+	float EffectValue = bDecrease ? -Value : Value;
+	Areas.Add(FAreaEffect(bDecrease, EffectValue, Delay, InAreaRef));
+	// apply new effect
+	LaunchEffect(EffectValue, Delay);
+}
+
+void UFlameComponent::LaunchEffect(float Value, float Delay)
+{
+	GetWorld()->GetTimerManager().SetTimer(
+	FlameEffectTimer, 
+	[this, Value]()
+	{
+		UpdateFlameValue(Value);
+	},
+	Delay, 
+	true
+	);
+}
+
+void UFlameComponent::EndEffect(AArea* InAreaRef)
+{
+	GetWorld()->GetTimerManager().ClearTimer(FlameEffectTimer);
+
+	int32 IndexToRemove = Areas.IndexOfByPredicate(
+		[InAreaRef](const FAreaEffect& Effect)
+		{
+			return Effect.AreaRef == InAreaRef;
+		});
+
+	if (IndexToRemove != INDEX_NONE)
+	{
+		Areas.RemoveAt(IndexToRemove);
+	}
+
+	if (Areas.Num() > 0)
+	{
+		FAreaEffect PreviousArea = Areas.Last();
+		LaunchEffect(PreviousArea.Value, PreviousArea.Delay);
+	}
+
+	if (Areas.Num() == 0)
+	{
+		InitFlame();
+	}
+}
--- Source/PFE_Narrative/Public/Player/FlameComponent.h
+++ Source/PFE_Narrative/Public/Player/FlameComponent.h
@@ -3,16 +3,33 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "PaperSpriteComponent.h"
+//#include "PaperSpriteComponent.h"
+#include "Components/ActorComponent.h"
 #include "FlameComponent.generated.h"
 
 
+class AArea;
 class APFEGameMode;
+
+USTRUCT()
+struct FAreaEffect
+{
+	GENERATED_BODY()
+	
+	bool bDecrease;
+	float Value;
+	float Delay;
+	TObjectPtr<AArea> AreaRef;
+
+	FAreaEffect() : bDecrease(false), Value(0.0f), Delay(0.0f), AreaRef(nullptr) {}
+	FAreaEffect(bool InDecrease, float InValue, float InDelay, AArea* InAreaRef) :
+		bDecrease(InDecrease), Value(InValue), Delay(InDelay), AreaRef(InAreaRef) {}
+};
 /**
  * 
  */
-UCLASS()
-class PFE_NARRATIVE_API UFlameComponent : public UPaperSpriteComponent
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PFE_NARRATIVE_API UFlameComponent : public UActorComponent
 {
 	GENERATED_BODY()
 	
@@ -24,15 +41,29 @@ public:
 	UFUNCTION(BlueprintCallable)
 	float GetFlameValue()  const { return CurrentFlameValue; }
 	
-protected:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Flame Properties")
-	float MaxFlameValue;
+	UFUNCTION()
+	void StartEffect(float Value, float Delay, bool bDecrease, AArea* AreaRef);
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Flame Properties")
+	UFUNCTION()
+	void EndEffect(AArea* AreaRef);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Flame Properties", meta = (AllowPrivateAccess = "true"))
+	float MaxFlameValue = 100.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Flame Properties", meta = (AllowPrivateAccess = "true"))
 	float CurrentFlameValue = 0.f;
 	
 	virtual void BeginPlay() override;
 
 	UFUNCTION()
 	void UpdateFlameValue(float Value);
+
+	FTimerHandle FlameEffectTimer;
+	
+	UFUNCTION()
+	void LaunchEffect(float Value, float Delay);
+
+private:
+	TArray<FAreaEffect> Areas;
 };
