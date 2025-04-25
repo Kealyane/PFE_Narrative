5563540802649183dabf48d0be51f15c0c49d213
Laurene <laurene.broquet@gmail.com>
2025-04-25 16:19:43 +0200

    add PFECharacterMovementComponent

---
+++ Source/PFE_Narrative/Private/Player/PFECharacterMovementComponent.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/PFECharacterMovementComponent.h"
+
+UPFECharacterMovementComponent::UPFECharacterMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
