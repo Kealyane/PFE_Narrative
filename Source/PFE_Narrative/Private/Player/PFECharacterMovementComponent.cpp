fb9b9757ab1dadfee9924e408f815b546fe20a82
Laurene <laurene.broquet@gmail.com>
2025-04-25 16:23:57 +0200

    add dash in movement component

--- Source/PFE_Narrative/Private/Player/PFECharacterMovementComponent.cpp
+++ Source/PFE_Narrative/Private/Player/PFECharacterMovementComponent.cpp
@@ -7,3 +7,77 @@ UPFECharacterMovementComponent::UPFECharacterMovementComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 }
+
+void UPFECharacterMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	PFECharacterOwner = Cast<APFECharacter>(GetOwner());
+	check(PFECharacterOwner);
+}
+void UPFECharacterMovementComponent::PhysCustom(float deltaTime, int32 Iterations)
+{
+	switch ((EPFEMovementMode)CustomMovementMode)
+	{
+	case EPFEMovementMode::PFEMOVE_DASHING:
+		PhysDash(deltaTime, Iterations);
+		break;
+	default:
+		Super::PhysCustom(deltaTime, Iterations);
+		break;
+	}
+}
+
+void UPFECharacterMovementComponent::OnMovementModeChanged(EMovementMode PreviousMovementMode, uint8 PreviousCustomMode)
+{
+	Super::OnMovementModeChanged(PreviousMovementMode, PreviousCustomMode);
+
+	if (MovementMode == MOVE_Walking)
+	{
+		DashCountAir = 0;
+		if (!bCanDash)
+		{
+			GetWorld()->GetTimerManager().SetTimer(DashCooldownHandle, this, &UPFECharacterMovementComponent::ResetDash, DashCooldown, false);
+		}
+	}
+}
+
+void UPFECharacterMovementComponent::PhysDash(float DeltaTime, int32 Iterations)
+{
+	Velocity = DashDirection * (DashDistance/DashDurationInSec);
+
+	FHitResult Hit;
+	SafeMoveUpdatedComponent(Velocity * DeltaTime, UpdatedComponent->GetComponentQuat(), true, Hit);
+
+	if (Hit.IsValidBlockingHit()) StopDash();
+}
+
+void UPFECharacterMovementComponent::StartDash(const FVector& InDirection)
+{
+	if (!bCanDash) return;
+
+	const bool bIsInAir = !IsMovingOnGround();
+
+	if (bIsInAir && DashCountAir >= MaxDashInAir) return;
+
+	if (bIsInAir) DashCountAir++;
+	else bCanDash = false;
+
+	PFECharacterOwner->bIsDashing = true;
+	DashDirection = InDirection.GetSafeNormal();
+	SetMovementMode(MOVE_Custom, (uint8)EPFEMovementMode::PFEMOVE_DASHING);
+
+	FTimerHandle DashTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(DashTimerHandle, this, &UPFECharacterMovementComponent::StopDash, DashDurationInSec, false);
+}
+
+void UPFECharacterMovementComponent::StopDash()
+{
+	PFECharacterOwner->bIsDashing = false;
+	SetMovementMode(MOVE_Walking);
+}
+
+void UPFECharacterMovementComponent::ResetDash()
+{
+	bCanDash = true;
+}
--- Source/PFE_Narrative/Public/Player/PFECharacterMovementComponent.h
+++ Source/PFE_Narrative/Public/Player/PFECharacterMovementComponent.h
@@ -6,6 +6,20 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "PFECharacterMovementComponent.generated.h"
 
+// walk, navwalk, falling, swimming, flying, custom, max
+UENUM(BlueprintType)
+enum class EPFEMovementMode : uint8
+{
+	PFEMOVE_NONE		UMETA(Hidden),
+	// PFEMOVE_JUMP		UMETA(DisplayName="Jump"),
+	// PFEMOVE_DOUBLE_JUMP UMETA(DisplayName="Double Jump"),
+	PFEMOVE_DASHING		UMETA(DisplayName="Dashing"),
+	PFEMOVE_WALL_GRAB	UMETA(DisplayName="Wall Grab"),
+	PFEMOVE_WALL_JUMP	UMETA(DisplayName="Wall Jump"),
+	PFEMOVE_MAX			UMETA(Hidden),
+};
+
+class APFECharacter;
 UCLASS()
 class PFE_NARRATIVE_API UPFECharacterMovementComponent : public UCharacterMovementComponent
 {
@@ -13,4 +27,43 @@ class PFE_NARRATIVE_API UPFECharacterMovementComponent : public UCharacterMoveme
 
 public:
 	UPFECharacterMovementComponent();
+protected:
+	TObjectPtr<APFECharacter> PFECharacterOwner;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement")
+	EPFEMovementMode CurrentMovementMode;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement|Dash",
+		meta = (AllowPrivateAccess = "true", ToolTip = "Direction of the dash"))
+	FVector DashDirection;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Dash",
+		meta = (AllowPrivateAccess = "true", ToolTip = "Distance to dash"))
+	float DashDistance = 1000.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Dash",
+		meta = (AllowPrivateAccess = "true", ToolTip = "Duration of the dash"))
+	float DashDurationInSec = 0.2f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Dash",
+		meta = (AllowPrivateAccess = "true", ToolTip = "Max Dash in the air"))
+	uint8 MaxDashInAir = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Dash",
+		meta = (AllowPrivateAccess = "true", ToolTip = "Dash cooldown"))
+	float DashCooldown = 0.7f;
+private:
+
+	// Dash
+	bool bCanDash = true;
+	uint8 DashCountAir = 0;
+	FTimerHandle DashCooldownHandle;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void PhysCustom(float deltaTime, int32 Iterations) override;
+	virtual void OnMovementModeChanged(EMovementMode PreviousMovementMode, uint8 PreviousCustomMode) override;
+
+	void PhysDash(float DeltaTime, int32 Iterations);
+public:
+	void StartDash(const FVector& InDirection);
+	void StopDash();
+	void ResetDash();
 };
