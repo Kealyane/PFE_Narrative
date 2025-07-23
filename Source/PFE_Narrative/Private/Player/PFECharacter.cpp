8efd2bbb415d868a46a930fc97047efebdc213b0
Laurene <laurene.broquet@gmail.com>
2025-07-23 12:42:40 +0200

    reset jump count at wall grab too

--- Source/PFE_Narrative/Private/Player/PFECharacter.cpp
+++ Source/PFE_Narrative/Private/Player/PFECharacter.cpp
@@ -300,7 +300,8 @@ void APFECharacter::Dash(const FInputActionValue& Value)
 void APFECharacter::WallGrabStart()
 {
 	if (bIsOnGround) return;
-	
+
+	JumpCount = 0;
 	bIsJumping = false;
 	bIsGrabbingWall = true;
 	PFEMovementComponent->StartWallGrab();
