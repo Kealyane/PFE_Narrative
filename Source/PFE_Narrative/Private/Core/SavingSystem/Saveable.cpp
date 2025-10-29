4446003d79c1a145497a5eb2ed8e41f04ed7a3cd
Laurene Broquet <laurene.broquet@gmail.com>
2025-10-29 18:12:46 +0100

    create interface Saveable
    for actors that need to be saved

---
+++ Source/PFE_Narrative/Private/Core/SavingSystem/Saveable.cpp
@@ -0,0 +1,6 @@
+// copyright2025 Allen Broquet Lamouche Tamen. All rights reserved.
+
+
+#include "Core/SavingSystem/Saveable.h"
+
+// Add default functionality here for any ISaveable functions that are not pure virtual.
---
+++ Source/PFE_Narrative/Public/Core/SavingSystem/Saveable.h
@@ -0,0 +1,40 @@
+// copyright2025 Allen Broquet Lamouche Tamen. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Saveable.generated.h"
+
+UINTERFACE(MinimalAPI)
+class USaveable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PFE_NARRATIVE_API ISaveable
+{
+	GENERATED_BODY()
+
+public:
+	
+	// C++ only
+
+	// virtual void OnSave(FMemoryWriter& MemoryWriter) PURE_VIRTUAL(OnSave);
+	// virtual void OnLoad(FMemoryReader& MemoryReader) PURE_VIRTUAL(OnLoad);
+	// virtual FString GetActorID() PURE_VIRTUAL(GetActorID, return FString(););
+
+	// C++ and Blueprint
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Save")
+	void OnSave(TArray<uint8>& OutData);
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Save")
+	void OnLoad(const TArray<uint8>& InData);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Save")
+	FString GetActorID() const;
+};
