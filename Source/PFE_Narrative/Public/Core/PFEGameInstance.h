35191fe7ea73091dfe2cd3b30195e075037dcd1c
Laurene Broquet <laurene.broquet@gmail.com>
2025-11-03 18:59:12 +0100

    game instance : add audio variables

--- Source/PFE_Narrative/Public/Core/PFEGameInstance.h
+++ Source/PFE_Narrative/Public/Core/PFEGameInstance.h
@@ -16,6 +16,7 @@ class PFE_NARRATIVE_API UPFEGameInstance : public UGameInstance
 	GENERATED_BODY()
 
 public:
+	// SAVE
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	TObjectPtr<UPFESaveGame> CurrentSave;
 
@@ -25,13 +26,26 @@ public:
 	void LoadGameDatasSync();
 	UFUNCTION(BlueprintCallable)
 	void SaveGameDatasASync();
+	UFUNCTION(BlueprintCallable)
+	bool CheckSaveFile();
 
 	UFUNCTION(BlueprintCallable)
 	void RegisterToSave(AActor* Actor) { ActorsToSave.Add(Actor); }
 	UFUNCTION(BlueprintCallable)
 	void UnregisterFromSave(AActor* Actor) { ActorsToSave.Remove(Actor); }
+
+	// AUDIO
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+	float SoundLevelMusic = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+	float SoundLevelSFX = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+	float SoundLevelAmbiance = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+	float SoundLevelMaster = 1.f;
 	
 private:
+	// SAVE
 	const FString SlotName = "Slot01";
 	const int32 UserIndex = 0;
 
