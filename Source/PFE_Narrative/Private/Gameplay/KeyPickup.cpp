ae8a3473c47624dc632f8c6f6123a51fdedf87d4
Laurene Broquet <laurene.broquet@gmail.com>
2025-11-19 18:10:29 +0100

    key load : implements isaveable

--- Source/PFE_Narrative/Private/Gameplay/KeyPickup.cpp
+++ Source/PFE_Narrative/Private/Gameplay/KeyPickup.cpp
@@ -4,30 +4,64 @@
 #include "Gameplay/KeyPickup.h"
 
 #include "PaperSpriteComponent.h"
+#include "Core/PFEGameInstance.h"
 #include "Core/PFEGameMode.h"
-#include "Kismet/GameplayStatics.h"
+#include "Core/SavingSystem/UniqueIDComponent.h"
 #include "Player/PFECharacter.h"
 
 AKeyPickup::AKeyPickup()
 {
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AKeyPickup::OnSave_Implementation(TArray<uint8>& OutData)
+{
+	FMemoryWriter Writer(OutData);
+	Writer << bHasBeenTaken;
+}
+
+void AKeyPickup::OnLoad_Implementation(const TArray<uint8>& InData)
+{
+	FMemoryReader Reader(InData);
+	Reader << bHasBeenTaken;
+
+	bHasBeenTaken ?	SetKeyVisibility(false) : SetKeyVisibility(true);
+}
+
+FString AKeyPickup::GetActorID_Implementation() const
+{
+	return UniqueIDComponent->ActorID;
 }
 
 void AKeyPickup::BeginPlay()
 {
 	Super::BeginPlay();
-	// if (APFEGameMode* PFEGameMode = Cast<APFEGameMode>(UGameplayStatics::GetGameMode(this)))
-	// {
-	// 	PFEGameMode->OnDeath.AddDynamic(this, &AKeyPickup::InitKey);
-	// }
-	InitKey();
+
+	if (UPFEGameInstance* PFE_GI = Cast<UPFEGameInstance>(GetWorld()->GetGameInstance()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("id : %s"), *GetActorID_Implementation())
+		PFE_GI->RegisterToSave(this);
+	}
 	
+	bHasBeenTaken = false;
+	SetKeyVisibility(true);
 	OnActorBeginOverlap.AddDynamic(this, &AKeyPickup::OnOverlapBegin);
 }
 
-void AKeyPickup::InitKey()
+void AKeyPickup::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	GetRenderComponent()->SetVisibility(true);
-	SetActorEnableCollision(true);
+	Super::EndPlay(EndPlayReason);
+	
+	if (UPFEGameInstance* PFE_GI = Cast<UPFEGameInstance>(GetWorld()->GetGameInstance()))
+	{
+		PFE_GI->UnregisterFromSave(this);
+	}
+}
+
+void AKeyPickup::SetKeyVisibility(bool Visible)
+{
+	GetRenderComponent()->SetVisibility(Visible);
+	SetActorEnableCollision(Visible);
 }
 
 void AKeyPickup::OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor)
@@ -36,15 +70,9 @@ void AKeyPickup::OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor)
 	{
 		if (APFECharacter* Character = Cast<APFECharacter>(OtherActor))
 		{
-			Character->StoreKey();
-			SetActorEnableCollision(false);
-			GetRenderComponent()->SetVisibility(false);
-			// FTimerHandle GrabHandle;
-			// GetWorldTimerManager().SetTimer(
-			// 	GrabHandle,
-			// 	[this]() { GetRenderComponent()->SetVisibility(false); },
-			// 	0.5f,
-			// 	false);
+			bHasBeenTaken = true;
+			Character->StoreKey(1);
+			SetKeyVisibility(false);
 		}
 	}
 }
--- Source/PFE_Narrative/Public/Gameplay/KeyPickup.h
+++ Source/PFE_Narrative/Public/Gameplay/KeyPickup.h
@@ -4,23 +4,41 @@
 
 #include "CoreMinimal.h"
 #include "PaperSpriteActor.h"
+#include "Core/SavingSystem/Saveable.h"
 #include "KeyPickup.generated.h"
 
+class UUniqueIDComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FLoadDoneDelegate, bool, bTaken);
 /**
  * 
  */
 UCLASS()
-class PFE_NARRATIVE_API AKeyPickup : public APaperSpriteActor
+class PFE_NARRATIVE_API AKeyPickup : public APaperSpriteActor, public ISaveable
 {
 	GENERATED_BODY()
 
 public:
 	AKeyPickup();
 
+	virtual void OnSave_Implementation(TArray<uint8>& OutData) override;
+	virtual void OnLoad_Implementation(const TArray<uint8>& InData) override;
+	virtual FString GetActorID_Implementation() const override;
+
+	UFUNCTION(BlueprintCallable, Category="Save")
+	void SetUniqueIDComp(UUniqueIDComponent* InUniqueIDComponent) { UniqueIDComponent = InUniqueIDComponent; }
+	
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	UFUNCTION()
-	void InitKey();
+	void SetKeyVisibility(bool Visible);
 	UFUNCTION()
 	void OnOverlapBegin(AActor* OverlappedActor, AActor* OtherActor);
+
+	UPROPERTY(BlueprintAssignable)
+	FLoadDoneDelegate LoadDone;
+
+private:
+	TObjectPtr<UUniqueIDComponent> UniqueIDComponent;
+	bool bHasBeenTaken;
 };
